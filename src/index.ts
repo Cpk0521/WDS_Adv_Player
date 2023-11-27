62ef9bf452a54c841cdf264209a6fe518fb0653b
CPK <samkit157@gmail.com>
2023-11-27 21:48:09 +0800

    clear

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import * as PIXI from "pixi.js";
-import '@pixi-spine/loader-uni'
-// import { Spine } from '@pixi-spine/runtime-4.1'
-// import { getUrlParams } from './utils/UrlParams'
+import { getUrlParams } from './utils/UrlParams'
 import { AdvPlayer } from "./AdvPlayer"
-// import { FontSizes } from "./types/Episode";
 
 function createApp() {
     if (document.getElementById("WDS")) {
@@ -40,60 +37,22 @@ function createApp() {
     return pixiapp
 }
 
-// const { id } = getUrlParams('id');
+const { id } = getUrlParams('id');
 
 const app = createApp();
 
-const check_texture = await PIXI.Assets.load('./UI.png');
-const check = new PIXI.Sprite(check_texture)
-app.stage.addChild(check)
-check.x = app.screen.width / 2
-check.y = app.screen.height / 2
-check.anchor.set(0.5)
+// for Test
+// const check_texture = await PIXI.Assets.load('./UI.png');
+// const check = new PIXI.Sprite(check_texture)
+// app.stage.addChild(check)
+// check.x = app.screen.width / 2
+// check.y = app.screen.height / 2
+// check.anchor.set(0.5)
 
 const advplayer = AdvPlayer.new();
 advplayer.addTo(app.stage);
 
-// if(id){
-//     advplayer.loadAndPlayEpisode(`https://raw.githubusercontent.com/WDS-Sirius/WDS_Adv_Resources/main/episode/${id}.json`);
-//     // advplayer.playEpisode()
-// }
-
-advplayer.loadAndPlay('2000101');
-
-
-// PIXI.Assets.setPreferences({
-//     preferCreateImageBitmap: false,
-//     // preferWorkers: false
-// });
-
-// const scale = app.screen.width / 800 * 0.35;
-
-
-// const bg_texture = await PIXI.Assets.load('https://raw.githubusercontent.com/WDS-Sirius/WDS_Adv_Resources/main/background/250.png');
-// const bg = new PIXI.Sprite(bg_texture)
-// app.stage.addChild(bg)
-// bg.x = app.screen.width / 2
-// bg.y = app.screen.height / 2
-// bg.anchor.set(0.5)
-// bg.scale.set(1.16)
-
-// const spine_02_data = await PIXI.Assets.load('https://raw.githubusercontent.com/WDS-Sirius/WDS_Adv_Resources/main/spine/30102.skel');
-// const spine_02 = new Spine(spine_02_data.spineData)
-// app.stage.addChild(spine_02)
-// spine_02.x = app.screen.width * 0.5;
-// spine_02.y = app.screen.height;
-// spine_02.scale.set(.75)
-// spine_02.state.setAnimation(0, "breath", true);
-// spine_02.state.setAnimation(1, "body/unique6", false);
-
-// const spine_03_data = await PIXI.Assets.load('https://raw.githubusercontent.com/WDS-Sirius/WDS_Adv_Resources/main/spine/10101.skel');
-// const spine_03 = new Spine(spine_03_data.spineData)
-// app.stage.addChild(spine_03)
-// // console.log(spine_03.getBounds())
-// spine_03.x = app.screen.width * 0.5;
-// spine_03.y = app.screen.height;
-// spine_03.scale.set(.8)
-// spine_03.state.setAnimation(0, "breath", true);
-// spine_03.state.setAnimation(1, "body/unique6", false);
-
+// advplayer.loadAndPlay('2000101');
+if(id){
+    advplayer.loadAndPlay(id);
+}
